21de50c9524027b51fe48880a4e2ed83c0671257
Dustin L. Dodson <dustinleedodson@gmail.com>
2015-10-15 09:47:39 -0800

    Attempting to load test page directly in js file.

--- util.js
+++ util.js
@@ -1,3 +1,6 @@
 function getPage(url, idName) {
 	$(idName).load(url); 
 }
+
+/*FILL TEST PAGE*/
+getPage('https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/master/raw/README', '#test');
