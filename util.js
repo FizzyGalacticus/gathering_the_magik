dddfb95c2221a712d61846a313f568c67566a954
Dustin L. Dodson <dustinleedodson@gmail.com>
2015-10-16 14:58:42 -0800

    Attempting to split into multiple html files.

--- util.js
+++ util.js
@@ -1,6 +1,8 @@
-function getPage(url, idName) {
-	$(idName).load(url); 
+function getPage(id, url) {
+	$('#' + id).load(url); 
 }
 
 /*FILL TEST PAGE*/
-getPage('https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/master/raw/README', '#test');
+getPage('analysis', 'https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/test/raw/analysis.html');
+getPage('contributions', 'https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/test/raw/contributions.html');
+getPage('sources', 'https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/test/raw/sources.html');
