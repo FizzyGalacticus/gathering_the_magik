b71f78d1abe6e932533932cc0e939392aba84f04
Dustin L. Dodson <dustinleedodson@gmail.com>
2015-10-16 15:12:29 -0800

    yet another test with JS in index.html.

--- util.js
+++ util.js
@@ -4,6 +4,3 @@ function getPage(id, url) {
 }
 
 /*FILL TEST PAGE*/
-getPage('analysis', 'https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/test/raw/analysis.html');
-getPage('contributions', 'https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/test/raw/contributions.html');
-getPage('sources', 'https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/test/raw/sources.html');
