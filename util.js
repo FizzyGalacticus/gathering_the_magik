4cb6e1a06cf4d3613b078c3a0aa053c981715b30
Dustin L. Dodson <dustinleedodson@gmail.com>
2015-10-16 17:28:11 -0800

    Moved alert to happen sooner...

--- util.js
+++ util.js
@@ -2,7 +2,7 @@ function getPage(id) {
 	mainUrl = 'https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/master/raw/resources/pages/';
 	mainUrl += (id+'.html');
 
+	alert('Trying to stuff contents of ' + mainUrl + ' into div: ' + id);
 	$('#' + id).load(mainUrl);
 
-	alert('Trying to stuff contents of ' + mainUrl + ' into div: ' + id);
 }
