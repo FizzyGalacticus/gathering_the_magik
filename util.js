744e8d592915effb42cafc11295dade5a2c6182e
Dustin L. Dodson <dustinleedodson@gmail.com>
2015-10-16 17:38:57 -0800

    Put a var in there!

--- util.js
+++ util.js
@@ -1,7 +1,7 @@
 function getPage(id) {
 	alert('Boo!');
-	mainUrl = 'https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/master/raw/resources/pages/';
-	mainUrl += (id+'.html');
+	var mainUrl = 'resources/pages/';
+	var mainUrl += (id +'.html');
 
 	alert('Trying to stuff contents of ' + mainUrl + ' into div: ' + id);
 	$('#' + id).load(mainUrl);
