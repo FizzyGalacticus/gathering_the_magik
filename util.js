64bdb551662da33d94a7b666cd772ab83f5372b7
Dustin L. Dodson <dustinleedodson@gmail.com>
2015-10-16 17:22:49 -0800

    Testing new JS feature...

--- util.js
+++ util.js
@@ -1,3 +1,6 @@
-function getPage(id, url) {
-	$('#' + id).load(url);
+function getPage(id) {
+	mainUrl = 'https://projects.cs.uaf.edu/redmine/projects/cs371_f15_gathering_the_magik/repository/revisions/master/raw/resources/pages/';
+	mainUrl += (id+'.html');
+
+	$('#' + id).load(mainUrl);
 }
