470ccc6705f3f8a4ac4f4856d2d9faa7fc7deee2
Dustin L. Dodson <dustinleedodson@gmail.com>
2015-10-16 17:47:58 -0800

    Es macht.

--- util.js
+++ util.js
@@ -1,9 +1,6 @@
 function getPage(id) {
-	alert('Boo!');
 	var mainUrl = 'resources/pages/';
 	var mainUrl = mainUrl + id +'.html';
 
-	alert('Trying to stuff contents of ' + mainUrl + ' into div: ' + id);
 	$('#' + id).load(mainUrl);
-
 }
