4ac1ec1e35e5fefcf03d3ff7622111e917b0fe15
Dustin L. Dodson <dustinleedodson@gmail.com>
2015-10-19 14:32:49 -0800

    Added function to 'login' as premium user.

--- util.js
+++ util.js
@@ -1,6 +1,25 @@
+var premium = false;
+
 function getPage(id) {
 	var mainUrl = 'resources/pages/';
 	var mainUrl = mainUrl + id + '.html';
 
 	$('#' + id).load(mainUrl);
 }
+
+var keystrokes = "";
+$(document).on("keypress", function(e) {
+	keytyped = String.fromCharCode(e.which);
+	console.log("Key pressed: " + keytyped);
+	e.preventDefault();
+
+	keystrokes = keystrokes + keytyped;
+	if(keystrokes.length > 6) {
+		keystrokes = keystrokes.substring(1,7);
+	}
+
+	if(keystrokes == "imawesome") {
+		alert("Woohoo! You're a premium user!");
+		premium = true;
+	}
+});
