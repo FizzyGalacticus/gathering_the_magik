b3f52d1027e9b928f245fe3a5baaee547b44977d
Dustin L. Dodson <dustinleedodson@gmail.com>
2015-10-19 15:09:43 -0800

    Another js test.

--- util.js
+++ util.js
@@ -1,3 +1,4 @@
+// var listener = new window.keypress.Listener();
 var premium = false;
 
 function getPage(id) {
@@ -7,6 +8,11 @@ function getPage(id) {
 	$('#' + id).load(mainUrl);
 }
 
+// listener.sequence_combo("i m a w e s o m e", function() {
+// 	premium = true;
+// 	alert("Woohoo! You're a premium user!");
+// });
+
 var keystrokes = "";
 $(document).on("keypress", function(e) {
 	keytyped = String.fromCharCode(e.which);
@@ -14,12 +20,11 @@ $(document).on("keypress", function(e) {
 	e.preventDefault();
 
 	keystrokes = keystrokes + keytyped;
-	if(keystrokes.length > 6) {
-		keystrokes = keystrokes.substring(1,7);
+	if(keystrokes.length > 9) {
+		keystrokes = keystrokes.substring(1,10);
 	}
 
-	if(keystrokes == "imawesome") {
-		alert("Woohoo! You're a premium user!");
-		premium = true;
+	if(keystrokes === "imawesome") {
+		alert("Woohoo! You typed in the secret code!");
 	}
 });
